a7822074333cf7d822b1f5fe4b0ad8ba104fbce6
onaroll2021 <lukeli.onaroll@gmail.com>
2022-09-20 02:10:52 +0000

    add info

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👏 assertion Passed:" + actual + "===" + expected);
+  }
+  if (actual === expected) {
+    console.log("🙀 assertion Failed:" + actual + "!==" + expected);
+  }
+};
+
+//TEST CODE
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1,1);
